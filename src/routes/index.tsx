792cab0d55dc4b5cec7e65b8352563f231d4abfe
tassadit-rgb <tassadit.cherfaoui@gmail.com>
2026-09-08 17:02:23 +0200

    Use Hostinger-served hero video on homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { SiteLayout, Section, BookButton } from "@/components/site/site-layout";
 import { FourCFramework } from "@/components/site/four-c";
 import { IntegralValues } from "@/components/site/integral-values";
 import { FounderSignature } from "@/components/site/founder-signature";
-import heroVideo from "@/assets/hero-lavender.mp4.asset.json";
 import heroBrand from "@/assets/hero-brand.jpg";
 import humanLeader from "@/assets/human-leader.jpg";
 import humanHealing from "@/assets/human-healing.jpg";
@@ -107,7 +106,7 @@ function HomePage() {
       <section className="relative overflow-hidden">
         <video
           className="absolute inset-0 h-full w-full object-cover"
-          src={heroVideo.url}
+          src="/Hero-You%E2%80%99re%20not%20Alone%3B.mp4"
           poster={heroBrand}
           autoPlay
           muted
